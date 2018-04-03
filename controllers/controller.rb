a9cd9575f1956a230a98c0d7e16d14c8876d125f
lavertyaf <lavertyaileen@gmail.com>
2018-04-03 16:51:45 +0100

    mvp completed

--- controllers/controller.rb
+++ controllers/controller.rb
@@ -8,10 +8,6 @@ get '/' do
   erb(:index)
 end
 
-get '/reorder' do
-  "Hello World"
-end
-
 post '/reorder' do
   Album.update_stock(params["id"].to_i, params["quantity"].to_i)
   erb(:increased_stock)
